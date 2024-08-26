b6df56a79048771087483e5ae45f0f496ea88e80
Astral <47200474+Astral-Haze@users.noreply.github.com>
2024-08-26 08:39:35 -0500

    Add files via upload

---
+++ unnamedpack.mrpack/overrides/kubejs/server_scripts/Interaction/Items_Eaten_Croptopia.js
@@ -0,0 +1,46 @@
+//requires: croptopia
+function bowl(food) {
+ItemEvents
+	.foodEaten(food, event => {
+		event.player.giveInHand('minecraft:bowl')
+	})
+}
+	bowl('croptopia:popcorn')
+	bowl('croptopia:baked_beans')
+	bowl('croptopia:mango_ice_cream')
+	bowl('croptopia:pecan_ice_cream')
+	bowl('croptopia:strawberry_ice_cream')
+	bowl('croptopia:vanilla_ice_cream')
+	bowl('croptopia:salsa')
+	bowl('croptopia:artichoke_dip')
+	bowl('croptopia:steamed_rice')
+	bowl('croptopia:onion_rings')
+	bowl('croptopia:chicken_and_noodles')
+	bowl('croptopia:chicken_and_dumplings')
+	bowl('croptopia:tofu_and_dumplings')
+	bowl('croptopia:spaghetti_squash')
+	bowl('croptopia:chicken_and_rice')
+	bowl('croptopia:cashew_chicken')
+	bowl('croptopia:rum_raisin_ice_cream')
+	bowl('croptopia:carnitas')
+	bowl('croptopia:crema')
+	bowl('croptopia:refried_beans')
+	bowl('croptopia:whipping_cream')
+	bowl('croptopia:ajvar')
+	bowl('croptopia:beef_stew')
+	bowl('croptopia:buttered_green_beans')
+	bowl('croptopia:chocolate_ice_cream')
+	bowl('croptopia:kiwi_sorbet')
+	bowl('croptopia:nether_wart_stew')
+	bowl('croptopia:potato_soup')
+	bowl('croptopia:ratatouille')
+	bowl('croptopia:steamed_broccoli')
+	bowl('croptopia:steamed_green_beans')
+	bowl('croptopia:pumpkin_soup')
+	bowl('croptopia:borscht')
+	bowl('croptopia:goulash')
+	bowl('croptopia:beetroot_salad')
+	
+/*
+
+*/
